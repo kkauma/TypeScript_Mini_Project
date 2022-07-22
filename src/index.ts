301580317ce46d26b197940f4e47e26385500b91
kkauma <kylekauma@gmail.com>
2022-07-22 14:42:21 -0500

    add form and wrap it around the input and button elements

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const btn = document.getElementById("btn")!; // add "!", guaranteed to not be null
+const btn = document.getElementById("btn")! as HTMLButtonElement; // add "!", guaranteed to not be null
 const input = document.getElementById("todoinput")! as HTMLInputElement; // Insist that this element exists
 // Need to tell TS that the above element is an HTMLInputElement
 
