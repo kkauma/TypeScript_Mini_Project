f17299bad738c1be7a9256d79232e3053b338fc1
kkauma <kylekauma@gmail.com>
2022-07-22 14:47:18 -0500

    add submit event for form

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,12 @@
 const btn = document.getElementById("btn"); // add "!", guaranteed to not be null
 const input = document.getElementById("todoinput"); // Insist that this element exists
 // Need to tell TS that the above element is an HTMLInputElement
-btn.addEventListener("click", () => {
-    alert(input.value);
-    input.value = "";
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("Submitted!");
 });
+// btn.addEventListener("click", () => {
+//   alert(input.value);
+//   input.value = "";
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement; // add "!", guaranteed to not be null
 const input = document.getElementById("todoinput")! as HTMLInputElement; // Insist that this element exists
 // Need to tell TS that the above element is an HTMLInputElement
+const form = document.querySelector("form")!;
 
-btn.addEventListener("click", () => {
-  alert(input.value);
-  input.value = "";
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Submitted!");
 });
+
+// btn.addEventListener("click", () => {
+//   alert(input.value);
+//   input.value = "";
+// });
