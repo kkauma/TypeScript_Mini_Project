f7ec8d1f4aa841274453ad3a876f63349b06622b
kkauma <kylekauma@gmail.com>
2022-07-22 14:56:18 -0500

    create checkbox and build basic functionality for todo list

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,17 @@ const btn = document.getElementById("btn"); // add "!", guaranteed to not be nul
 const input = document.getElementById("todoinput"); // Insist that this element exists
 // Need to tell TS that the above element is an HTMLInputElement
 const form = document.querySelector("form");
+const list = document.getElementById("todolist");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("Submitted!");
+    const newTodoText = input.value;
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value = "";
 });
 // btn.addEventListener("click", () => {
 //   alert(input.value);
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ const btn = document.getElementById("btn")! as HTMLButtonElement; // add "!", gu
 const input = document.getElementById("todoinput")! as HTMLInputElement; // Insist that this element exists
 // Need to tell TS that the above element is an HTMLInputElement
 const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("Submitted!");
+  const newTodoText = input.value;
+  const newLI = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLI.append(newTodoText);
+  newLI.append(checkbox);
+  list.append(newLI);
+  input.value = "";
 });
 
 // btn.addEventListener("click", () => {
